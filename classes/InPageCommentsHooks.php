36dd6aeeeab6aabd5d46c2c3a779447a479cfe76
uta <service.yk+github@gmail.com>
2014-10-30 10:16:26 +0900

    Change request method from http post to ajax api

--- classes/InPageCommentsHooks.php
+++ classes/InPageCommentsHooks.php
@@ -3,7 +3,7 @@ if(!defined('MEDIAWIKI')) die;
 class InPageCommentsHooks {
   public static function renderCommentForm($text, $params=array(), $parser=null, $frame=false) {
     global $wgInPageCommentsFormMessage, $wgInPageCommentsMaxLetters;
-    return Xml::openElement('form', array('action'=>SpecialPage::getTitleFor('InPageComments')->getLocalURL(), 'method'=>'post', 'id'=>'in_page_comments_form', 'class'=>'integrated'))
+    return Xml::openElement('form', array('action'=>'/', 'method'=>'post', 'id'=>'in_page_comments_form', 'class'=>'integrated'))
          . Xml::input('t', false, $parser->mTitle->getDBKey(), array('type'=>'hidden'))
          . Xml::openElement('button', array('type'=>'submit', 'id'=>'in_page_comments_btn', 'class'=>'c_bg1 c_border1'))
          . Xml::openElement('span', array('class'=>'fa fa-comment-o'))
